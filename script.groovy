0a0c06fdab083378876e688fa7dc6004a31a7ace
kirana4321 <35378122+kirana4321@users.noreply.github.com>
2020-10-12 23:43:56 -0700

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,14 @@
+def buildApp(){
+    echo "Groovy script to Build an application"
+}
+
+def testApp(){
+    echo "Groovy script to Test an application"
+}
+
+
+def deployApp(){
+    echo "Groovy script to deploy an application version ${params.VERSION}""
+}
+
+return this
